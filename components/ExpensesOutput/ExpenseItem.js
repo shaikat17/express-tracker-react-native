b5663c88f4b4d343badc8fac39a6ba8b918d20cd
shaikat17 <shaikatpal56@gmail.com>
2024-11-01 23:46:49 +0600

    Add: date formatter script added.

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,12 +1,13 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native'
 import { GlobalStyles } from '../../constants/styles'
+import { getFormattedDate } from '../../util/date'
 const ExpenseItem = ({description, amount, date}) => {
   return (
       <Pressable>
           <View style={styles.expenseItem}>
               <View>
                   <Text style={[ styles.textBase, styles.description]}>{description}</Text>
-                  <Text style={styles.textBase}>{date.toString()}</Text>
+                  <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
               </View>
               <View style={styles.amountContainer}>
               <Text style={styles.amount}>${amount}</Text>
