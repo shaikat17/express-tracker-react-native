4bbb2377959e0029d3d901bb6e5cf52278532c6b
shaikat17 <shaikatpal56@gmail.com>
2024-11-03 00:11:06 +0600

    Working on button delete and update functionality.

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,10 @@
 import { useLayoutEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { GlobalStyles } from "../constants/styles";
+
+import { Ionicons } from '@expo/vector-icons';
+import Button from "../components/UI/Button";
+
 const ManageExpense = ({ route, navigation }) => {
     const id = route.params?.expenseId;
     const isEditing = !!id;
@@ -10,10 +15,46 @@ const ManageExpense = ({ route, navigation }) => {
         })
     }, [navigation, isEditing])
 
+  function deleteExpenseHandler() { }
+
+  function cancelHandler() { }
+
+  function confirmHandler() { }
+  
   return (
-    <View>
-      <Text>ManageExpense</Text>
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode='flat' onPress={cancelHandler}>Cancel</Button>
+        <Button style={styles.button} onPress={confirmHandler}>{ isEditing ? 'Update' : 'Add' }</Button>
+      </View>
+      {isEditing && <View style={styles.deleteContainer}>
+        <Ionicons name="trash" size={36} color={GlobalStyles.colors.error500} onPress={deleteExpenseHandler} />
+      </View>}
     </View>
   );
 };
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  }, 
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center'
+  }
+});
